c466028f8deb141a6c1fef5109718410eab4fcdf
daodao97 <daodao97@foxmail.com>
2024-10-18 18:32:27 +0800

    up

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM golang:1.22 AS builder
+
+ARG BUILD_VERSION
+
+WORKDIR /build
+
+COPY . .
+RUN go mod download
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w -s" -o app .
+
+FROM alpine:latest AS final
+
+WORKDIR /app
+COPY --from=builder /build/app /app/
+
+RUN apk update && \
+    apk add --no-cache sudo tzdata
+
+# 设置所需的时区，例如亚洲/上海
+ENV TZ=Asia/Shanghai
+
+EXPOSE 8001
+
+# 创建软链接，指向你想要的时区文件
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+ENTRYPOINT ["/app/app"]
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/ping", pingHandler)
+
+	err := http.ListenAndServe(":8001", nil)
+	if err != nil {
+		fmt.Printf("服务器启动失败: %v\n", err)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	redisURL := os.Getenv("REDIS_URL")
+	fmt.Fprintf(w, "redisURL: %s", redisURL)
+
+	client := redis.NewClient(&redis.Options{
+		Addr: redisURL,
+	})
+
+	defer client.Close()
+
+	ctx := r.Context()
+	info, err := client.Info(ctx).Result()
+	if err != nil {
+		fmt.Printf("获取Redis信息失败: %v\n", err)
+		http.Error(w, "内部服务器错误", http.StatusInternalServerError)
+		return
+	}
+
+	// 从info字符串中提取Redis版本信息
+	var version string
+	for _, line := range strings.Split(info, "\n") {
+		if strings.HasPrefix(line, "redis_version:") {
+			version = strings.TrimPrefix(line, "redis_version:")
+			break
+		}
+	}
+
+	if version == "" {
+		fmt.Println("无法获取Redis版本信息")
+		http.Error(w, "无法获取Redis版本信息", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Redis版本: %s", version)
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "pong")
+}

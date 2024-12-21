db010c6ec6f3bcd74ae34ae631e06e74469e1e39
daodao97 <daodao97@foxmail.com>
2024-12-21 14:19:52 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# kamal-test
+
+
+```
+kamal init
+
+kamal setup
+
+kamal deploy
+
+kamal accessory boot redis
+
+kamal accessory reboot redis
+
+kamal app containers
+
+kamal rollback {git commit id}
+```
+
+
+.env 不起作用
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"kamal/pkg/conf"
+	"net/http"
+
+	"github.com/daodao97/xgo/xapp"
+	"github.com/daodao97/xgo/xredis"
+	"github.com/gin-gonic/gin"
+)
+
+var version string
+
+func main() {
+	app := xapp.NewApp().
+		AddStartup(
+			conf.Init,
+			func() error {
+				return xredis.Init(conf.GetRedis())
+			},
+		).
+		AddServer(xapp.NewHttp(":8001", h))
+
+	if err := app.Run(); err != nil {
+		fmt.Printf("Application error: %v\n", err)
+	}
+}
+
+func h() http.Handler {
+	e := xapp.NewGin()
+	e.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"version": version, "config": conf.Get()})
+	})
+	return e.Handler()
+}

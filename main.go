e3305508f65104329fd3fe28333a96967531cb01
daodao97 <daodao97@foxmail.com>
2024-12-21 13:59:57 +0800

    up

--- main.go
+++ main.go
@@ -29,8 +29,12 @@ func main() {
 
 func h() http.Handler {
 	e := xapp.NewGin()
-	e.GET("/ping", func(c *gin.Context) {
+	e.GET("/info", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"version": version})
+	})
+	e.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+		// c.JSON(http.StatusOK, gin.H{"version": version})
 	})
 	return e.Handler()
 }
